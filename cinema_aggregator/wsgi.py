cbaea6ec37f7d8883061f166077dc156d2a4dc73
Mudwin <nshmilsson@gmail.com>
2026-01-19 17:03:01 +0500

    fix: update wsgi file

--- cinema_aggregator/wsgi.py
+++ cinema_aggregator/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for cinema_aggregator project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from dotenv import load_dotenv 
 
-from django.core.wsgi import get_wsgi_application
+load_dotenv('/home/Mudwin/cinema-aggregator/.env') 
+
+path = '/home/Mudwin/cinema-aggregator'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinema_aggregator.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
